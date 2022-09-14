84e90d1e76a6829bb2084ae7953ab76ece02391a
hasnainkhalfan <73541651+hasnainkhalfan@users.noreply.github.com>
2022-09-14 16:05:39 -0400

    added user model and started thought controllers

--- models/Users.js
+++ models/Users.js
@@ -1,6 +1,43 @@
 const { Schema, model } = require('mongoose');
 
 // create user model
+const userSchema = new Schema(
+  {
+    username: {
+      type:  String,
+      required: true,
+      unique:true,
+      trim:true
+    },
+    email: {
+      type: String,
+      required: true, 
+      unique: true,
+      match: [/.+@.+\..+/, 'Must use a valid email address'],
+    },    
+    thoughts: [{
+        type: Schema.Types.ObjectId,
+       
+        ref: "Thought" ,
+      }],
+    friends: [{
+      type: Schema.Types.ObjectId,
+     
+      ref: "User" ,
+    }],
+
+  },
+  {
+   toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+userSchema.virtual('friendCount').get(function () {
+  return this.friends.length;
+});
 
 const User = model('User', userSchema);
 
